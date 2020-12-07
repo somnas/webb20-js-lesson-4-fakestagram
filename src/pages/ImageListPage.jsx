88f3731910cdaf5b3bf9a30755a6f1526233cb13
Hassan Mian <mian@willandskill.se>
2020-12-07 10:14:36 +0100

    Fetch data from API

--- src/pages/ImageListPage.jsx
+++ src/pages/ImageListPage.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ImageItem from '../components/ImageItem'
-import { imageList } from '../imageList'
+
 
 export default function ImageListPage() {
-  console.log(imageList)
+  const [imageList, setImageList] = useState([])
+
+  function handleOnClick() {
+    fetch("https://image-mock-data.firebaseio.com/images.json")
+    .then( response => response.json() )
+    .then( data => setImageList(data) )
+  }
+
   return (
     <>
       <h2>Instagram</h2>
+      <button onClick={handleOnClick}>Load Data</button>
       <div className="row">
         {imageList.map( imageItem => {
           return <ImageItem 
